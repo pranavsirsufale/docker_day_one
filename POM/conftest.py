ac1809e894efa4e24c0518c9b79309c5bbfe64aa
pranavsirsufale <pranavdada334@gmail.com>
2025-05-29 20:40:26 +0530

    add POM Directory

---
+++ POM/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from POM.conf.config import TestData  # Adjust import if needed
+
+@pytest.fixture(params=["chrome"], scope="class")
+def init_driver(request):
+    if request.param == "chrome":
+        service = Service(TestData.CHROME_EXECUTABLE_PATH)
+        driver = webdriver.Chrome(service=service)
+    elif request.param == "firefox":
+        service = Service(TestData.FIREFOX_EXECUTABLE_PATH)
+        driver = webdriver.Firefox(service=service)
+
+    request.cls.driver = driver
+    yield
+    driver.quit()
